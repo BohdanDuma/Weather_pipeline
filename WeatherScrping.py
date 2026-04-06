3cf842b0ab03078f2bd9e0437d28bfcc8718d672
BohdanDuma <duma-bohdan@ukr.net>
2026-04-06 13:21:57 +0300

    refactoring: rebild class WeatherDataClient

--- WeatherScrping.py
+++ WeatherScrping.py
@@ -1,40 +1,40 @@
 import requests
 
 class WeatherDataClient:
-    """
-    Клас для взаємодії з OpenWeatherMap API.
-    """
-    
-    def __init__(self, api_url: str):
-        self.url = api_url
-        self.dict_city = 
-    def _build_params(self, lat: float, lon: float) -> str:
-        pass
-
-    def fetch_raw_data(self, city_name: str) -> dict:
+    def __init__(self):
+        self.lat, self.lon, self.city = self._build_params()
+        self.timestamp, self.temp, self.windspeed =  self._fetch_raw_data()  
+    def __repr__(self):
+        return "{'timestamp': '2026-04-06 11:20:00','city': 'Lviv','temp': 8.3,'windspeed': 12.5,'lat': 49.8,'lon': 24.0}"
+    def _build_params(self) -> tuple:
+        try:
+            url_cordinates = 'https://ipapi.co/json/'
+            response_self_cord = requests.get(url_cordinates)
+            d_cord = response_self_cord.json()
+            lat = d_cord.get('latitude')
+            lon = d_cord.get('longitude')
+            city = d_cord.get('city')
+            if lat is None or lon is None:
+                raise ValueError('API not give cord')
+            return lat, lon, city
+        except Exception as e:
+            raise RuntimeError(f"Error geo: {e}")
+    def _fetch_raw_data(self) -> dict:
+        url = (f"https://api.open-meteo.com/v1/forecast?latitude={self.lat:.4f}&longitude={self.lon:.4f}&current_weather=true&timezone=auto")
         response = requests.get(url)
         data = response.json()
         temp = data['current_weather']['temperature']
-        print(temp)
-        pass
-
-    def get_weather_summary(self, city_name: str) -> dict:
+        timestamp = data['current_weather']['time']
+        windspeed = data['current_weather']['windspeed']
+        return timestamp, temp, windspeed
+        
+    def get_weather_summary(self):
         """
         Головний метод. Викликає fetch_raw_data та 
         фільтрує дані, залишаючи лише необхідні поля.
         """
         pass
-url = "https://api.open-meteo.com/v1/forecast?latitude=49.83&longitude=23.94&current_weather=true"
-    def _get_location(self):
         
-response = requests.get(url)
-data = response.json()
-temp = data['current_weather']['temperature']
-print(temp)
-url_cordinates = 'https://ipapi.co/json/'
-response_self_cord = requests.get(url_cordinates)
-d_cord = response_self_cord.json()
-lat = d_cord.get('latitude')
-lon = d_cord.get('longitude')
-city = d_cord.get('city')
-print(city)
+if __name__ == "__main__":
+    first = WeatherDataClient()
+    print(first)
