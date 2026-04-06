90e10dde8763925fa26b1987109bfd58940c258c
BohdanDuma <duma-bohdan@ukr.net>
2026-04-06 14:16:21 +0300

    fixed: cleaner code

--- WeatherScrping.py
+++ WeatherScrping.py
@@ -1,40 +1,51 @@
 import requests
-
+from datetime import datetime
 class WeatherDataClient:
     def __init__(self):
-        self.lat, self.lon, self.city = self._build_params()
-        self.timestamp, self.temp, self.windspeed =  self._fetch_raw_data()  
+        self.data = {} 
     def __repr__(self):
-        return "{'timestamp': '2026-04-06 11:20:00','city': 'Lviv','temp': 8.3,'windspeed': 12.5,'lat': 49.8,'lon': 24.0}"
-    def _build_params(self) -> tuple:
+        return (f"{self.data}")
+    def _fetch_location(self) -> tuple:
         try:
             url_cordinates = 'https://ipapi.co/json/'
             response_self_cord = requests.get(url_cordinates)
             d_cord = response_self_cord.json()
-            lat = d_cord.get('latitude')
-            lon = d_cord.get('longitude')
-            city = d_cord.get('city')
-            if lat is None or lon is None:
-                raise ValueError('API not give cord')
-            return lat, lon, city
+            return {
+                "lat": d_cord.get('latitude'),
+                "lon": d_cord.get('longitude'),
+                "city": d_cord.get('city')
+            }
+            
         except Exception as e:
-            raise RuntimeError(f"Error geo: {e}")
-    def _fetch_raw_data(self) -> dict:
-        url = (f"https://api.open-meteo.com/v1/forecast?latitude={self.lat:.4f}&longitude={self.lon:.4f}&current_weather=true&timezone=auto")
-        response = requests.get(url)
-        data = response.json()
-        temp = data['current_weather']['temperature']
-        timestamp = data['current_weather']['time']
-        windspeed = data['current_weather']['windspeed']
-        return timestamp, temp, windspeed
-        
+            print(f"Error geo: {e}")
+            return None 
+    def _fetch_raw_data(self, lat, lon) -> dict:
+        try:
+            url = (f"https://api.open-meteo.com/v1/forecast")
+            param = {"latitude": lat,
+                    "longitude": lon,
+                    "current_weather": "true",
+                    "timezone": "auto"
+                    }
+            response = requests.get(url, params=param, timeout=10)
+            return response.json().get('current_weather')
+        except Exception as e:
+            print(f"problem with weather extraction {e}")
+            return None
     def get_weather_summary(self):
-        """
-        Головний метод. Викликає fetch_raw_data та 
-        фільтрує дані, залишаючи лише необхідні поля.
-        """
-        pass
+        loc = self._fetch_location()
+        if loc is None:
+            print("--- Warning: Using hardcoded location (Lviv) ---")
+            loc = {"lat": 49.8383, "lon": 24.0232, "city": "Lviv (Manual)"}
+            
         
+        weather = self._fetch_raw_data(loc["lat"], loc["lon"])   
+        if not weather:
+            print("Can't extract weather")
+        self.data = {'timestamp': datetime.now().isoformat(),'city': loc['city'],'temp': weather.get('temperature'),'windspeed': weather.get('windspeed'),'lat': loc["lat"],'lon': loc["lon"]}
+        return self.data
 if __name__ == "__main__":
-    first = WeatherDataClient()
-    print(first)
+    
+    client = WeatherDataClient()
+    final_result = client.get_weather_summary()
+    print(final_result)
