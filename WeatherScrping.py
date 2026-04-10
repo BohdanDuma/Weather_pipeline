774497303921955f6e33b300351880e8631dfd6f
BohdanDuma <duma-bohdan@ukr.net>
2026-04-10 11:56:03 +0300

    added logging

--- WeatherScrping.py
+++ WeatherScrping.py
@@ -1,5 +1,7 @@
 import requests
 from datetime import datetime
+import time
+import logging
 class WeatherDataClient:
     def __init__(self):
         self.data = {} 
@@ -7,6 +9,7 @@ class WeatherDataClient:
         return (f"{self.data}")
     def _fetch_location(self) -> tuple:
         try:
+            logging.info('Try to take clients IP')
             url_cordinates = 'https://ipapi.co/json/'
             response_self_cord = requests.get(url_cordinates)
             d_cord = response_self_cord.json()
@@ -17,10 +20,11 @@ class WeatherDataClient:
             }
             
         except Exception as e:
-            print(f"Error geo: {e}")
+            logging.warning("Can't take IP for client location")
             return None 
     def _fetch_raw_data(self, lat, lon) -> dict:
         try:
+            logging.info("Conecting with free weather API")
             url = (f"https://api.open-meteo.com/v1/forecast")
             param = {"latitude": lat,
                     "longitude": lon,
@@ -28,20 +32,33 @@ class WeatherDataClient:
                     "timezone": "auto"
                     }
             response = requests.get(url, params=param, timeout=10)
-            return response.json().get('current_weather')
+            if response.status_code == 200:
+                logging.info("Succsessful conection to weather  API's")
+                return response.json().get('current_weather')
+            
+          
         except Exception as e:
-            print(f"problem with weather extraction {e}")
+            logging.warning(f"{e}Something wrong with acceptance from API")
             return None
     def get_weather_summary(self):
         loc = self._fetch_location()
         if loc is None:
-            print("--- Warning: Using hardcoded location (Lviv) ---")
+            logging.warning("--- Warning: Using hardcoded location (Lviv) ---")
             loc = {"lat": 49.8383, "lon": 24.0232, "city": "Lviv (Manual)"}
+        atemps = 0   
+        while atemps < 3:
+            atemps +=1
+            weather = self._fetch_raw_data(loc["lat"], loc["lon"])
             
+            if not weather:
+                logging.error(f"Attempt {atemps} failed")
+                if atemps < 3:
+                    time.sleep(30)
+                    continue
+                else:
+                    return None
+            self.data = {'timestamp': datetime.now().isoformat(),'lat': loc["lat"],'lon': loc["lon"],'city': loc['city'],'temp': weather.get('temperature'),'windspeed': weather.get('windspeed')}
+            logging.debug(f"had {self.data} info")
+            logging.info("Succsessful get data extraction from weather  API's")
+            return self.data
         
-        weather = self._fetch_raw_data(loc["lat"], loc["lon"])   
-        if not weather:
-            print("Can't extract weather")
-            return None
-        self.data = {'timestamp': datetime.now().isoformat(),'lat': loc["lat"],'lon': loc["lon"],'city': loc['city'],'temp': weather.get('temperature'),'windspeed': weather.get('windspeed')}
-        return self.data
--- proces.py
+++ proces.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import platform
-from WeatherScrping import WeatherDataClient
+import logging
 class DataProcessing:
     def __init__(self, dictionary):
         self.dictionary = dictionary
@@ -10,20 +10,18 @@ class DataProcessing:
         self.last_str = pd.DataFrame()
     def _load(self):
         self.df = pd.DataFrame([self.dictionary])
-        print(self.df.columns.tolist())
+        logging.debug(self.df.columns.tolist())
         try:
             self.last_str = pd.read_json('last_state.json', orient='records')
-            if self.last_str.empty:
-                self.state = False  
+        except FileNotFoundError:
+            logging.info("file of last state not found")
+            self.last_str = pd.DataFrame()
         except Exception as e:
-            print(f"{e} can't take json to extract last")
-            self.state = False
-        
+            logging.warning("Can't read JSON: {e}")
+            self.last_str = pd.DataFrame()
 
-        
     def _compare_cur_last(self):
-        print(f"Доступні колонки: {self.df.columns.tolist()}")
-        if not self.state:
+        if self.last_str.empty:
             return self.df
         cur_time = pd.to_datetime(self.df['timestamp'].iloc[-1])
         last_time = pd.to_datetime(self.last_str['timestamp'].iloc[-1])
@@ -34,7 +32,8 @@ class DataProcessing:
         cur_city = self.df['city'].iloc[-1]
         last_city = self.last_str['city'].iloc[-1]
         if cur_time_min == last_time_min and cur_temp == last_temp and cur_city == last_city:
-            return None #тут логінг
+            logging.info(f"Date for time: {cur_time} already exist")
+            return None 
         else:
             return self.df
          
@@ -43,14 +42,15 @@ class DataProcessing:
         if  self._compare_cur_last() is not None:
             self.df['windy'] = np.where(self.df['windspeed'] > 10, 'Yes', 'No')
             self.df['platform'] = platform.node()
+            logging.info("Added new columns (windy,platform)")
             return self.df
-        else:
-            return None
+        
+        return None
     def run_all(self):
         self._load()
         
         return self._add_new_columns()
 
-date_from_api = {'timestamp': '2026-04-09T21:37:34.286596', 'lat': 49.839, 'lon': 24.0191, 'city': 'Lviv', 'temp': 2.0, 'windspeed': 6.8}
+'''date_from_api = {'timestamp': '2026-04-09T21:37:34.286596', 'lat': 49.839, 'lon': 24.0191, 'city': 'Lviv', 'temp': 2.0, 'windspeed': 6.8}
 pandas_procesed = DataProcessing(date_from_api)
-final_pandas = pandas_procesed.run_all()
+final_pandas = pandas_procesed.run_all()'''
